6257e1f659eca348019b68f10d5b91cf0d5eadc5
LauraPosada <posadalaura998@gmail.com>
2017-08-21 20:41:20 -0500

    lista

--- app/src/main/java/com/example/camilo/tetengo/ListaLugares.java
+++ app/src/main/java/com/example/camilo/tetengo/ListaLugares.java
@@ -1,6 +1,8 @@
 package com.example.camilo.tetengo;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,19 +20,29 @@ public class ListaLugares extends AppCompatActivity {
     private ArrayList<String> datosLugares;
     private ArrayAdapter<String> adaptador;
 
+    SharedPreferences persistencia;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_lugares);
 
+        persistencia = getSharedPreferences("ubicaciones", Context.MODE_PRIVATE);
         listaLugares = (ListView) findViewById(R.id.listaLugares);
 
+        datosLugares = new ArrayList<String>();
 
+        String lugar1= persistencia.getString("nombreU"," ");
+        String desc = persistencia.getString("descU"," ");
 
+        String[] partsLugar = lugar1.split(", ");
+        String[] partsDesc = desc.split(", ");
 
-
-
-
+        for (int i =0; i<partsLugar.length;i++) {
+            datosLugares.add(partsLugar[i] + " - " + partsDesc[i]);
+        }
+        adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datosLugares);
+        listaLugares.setAdapter(adaptador);
 
         listaLugares.setOnItemClickListener(new AdapterView.OnItemClickListener(){
             @Override
